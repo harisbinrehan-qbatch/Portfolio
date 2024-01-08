e7fcfa83963e2cc5a18c143ea26e1caffa645972
harisbinrehan <haris.rehan@qbatch.com>
2024-01-08 17:25:48 +0500

    Typo mistakes removed

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -19,7 +19,7 @@ const AboutMe = () => {
               />
             </div>
             <div className="w-xs md:max-w-4xl">
-              <h1 className="text-4xl font-bold">Why You Are Hire Me</h1>
+              <h1 className="text-4xl font-bold">Why Hire Me ?</h1>
               <p className="py-6 md:pr-8">
                 As a MERN Stack Developer, I offer a unique blend of technical
                 prowess, creative problem-solving, and a passion for delivering
